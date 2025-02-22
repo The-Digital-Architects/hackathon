2eaad6fc68ca512152aa93705421a2b2aa9e4c11
Pawel024 <p.j.rok@student.tudelft.nl>
2025-02-22 11:55:31 +0100

    move model and add evaluate method

--- src/models.py
+++ src/models.py
@@ -1,13 +1,11 @@
 import joblib
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
-from sklearn.base import BaseEstimator
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, RandomForestRegressor
 from sklearn.neural_network import MLPClassifier
-
+import numpy as np
 
 class BaseModel:
-    def __init__(self, config = None):
-        self.config = config or {}
-        self.model: BaseEstimator = self._create_model()
+    def __init__(self):
+        self.model = self._create_model()
     
     def _create_model(self):
         raise NotImplementedError
@@ -44,3 +42,18 @@ class NeuralNetwork(BaseModel):
     def _create_model(self):
         params = self.config.get('model_params', {})
         return MLPClassifier(**params)
+
+class WildfirePredictor(BaseModel):
+    def _create_model(self, n_estimators=100, random_state=42):
+        return RandomForestRegressor(
+            n_estimators=n_estimators,
+            random_state=random_state
+        )
+    
+    def evaluate(self, X, y_true):
+        y_pred = self.predict(X)
+        N = len(y_true)
+        log_array = np.abs(np.log(y_pred/y_true))
+        constrained_log_array = np.min(log_array, 10)
+        sum_logs = np.sum(constrained_log_array)
+        return 1/N * sum_logs
